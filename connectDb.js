a9e171d543ea7c074915668339aa880d5c76c0e7
Bastin Robin <bastinrobins@gmail.com>
2013-11-25 23:58:43 +0530

    Features clear, createDB, removeRow, viewRow, createJSON, readJSON added successfully

--- connectDb.js
+++ connectDb.js
@@ -1,14 +1,17 @@
 /*
-	Author: Bastin Robin
-	Title: ConnectDb.js
-	Description: ConnectDb.js is an inbuild browser DB supported of HTML5 Offline APi
+ * ConnectDb.js
+ * Version: 0.1
+ * Description: ConnectDb.js is an inbuild browser DB supported of HTML5 Offline APi
+ * Copyright (c) 2013 Bastin Robins.J
+ * Project repository: https://github.com/bastinrobin/ConnectDB.js
+ * Dual licensed under GPL licenses.
 */
 
 (function () {
 	//Initialization of ConnectDB Framework
-	ConnectDb = {
+	connectDb = {
 		version : "0.1",
-		date : "26/Nov/2013",
+		date : "Nov-2013",
 
 		//Check if the browser supports ConnectDB
 		checkBrowser: function() {
@@ -22,8 +25,39 @@
 			if(localStorage.length > 0) {
 				return true;
 			} else { return false; }
+		},
+
+		//Clear any existing db completely
+		clear : function() {
+			localStorage.clear();
+			console.log('Cleared Successfully');
+		},
+
+		//Create New Collection or Db
+		createDb : function (key, value) {
+			localStorage.setItem(key, value);
+		},
+
+		//Remove a specific row
+		removeRow : function(key) {
+			localStorage.clear(key);
+		},
+
+		//View Complete Row by iteration key values
+		viewRow: function() {
+			for(key in localStorage) {
+				console.log(localStorage[key]);
+			}
+		},
+
+		//Create JSON Db or collection
+		createJSON: function(key, json) {
+			localStorage.setItem(key, JSON.stringify(json));
+		},
+
+		//Read JSON from Db or Collections
+		readJSON: function(key) {
+			return JSON.parse(localStorage.key)
 		}
-
-
 	}
 })(window);
