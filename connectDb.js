226f8d4c71a89e1def17283369b4186874b0b3df
Bastin Robin <bastinrobins@gmail.com>
2013-11-27 12:45:26 +0530

    Made vital changes remove minifed for a while

--- connectDb.js
+++ connectDb.js
@@ -4,7 +4,6 @@
  * Description: ConnectDb.js is an inbuild browser DB supported of HTML5 Offline APi
  * Copyright (c) 2013 Bastin Robins.J
  * Project repository: https://github.com/bastinrobin/ConnectDB.js
- * Dual licensed under GPL licenses.
 */
 
 (function () {
@@ -19,66 +18,54 @@
 				return true; 
 			} else { return false; }  
 		},
-
 		//Check if the any db exist
 		isempty: function() {
 			if(localStorage.length == 0) {
 				return true;
 			} else { return false; }
 		},
-
 		//Clear any existing db completely
 		clear : function() {
 			localStorage.clear();
 			return true;
 		},
-
 		//Create New Collection or Db
 		createRow : function (key, value) {
 			localStorage.setItem(key, value);
 		},
-
 		//Retrieve a JSON with key
 		getRow : function (key) {
 			return localStorage.getItem(key);
 		},
-
 		//Remove a specific row
 		removeRow : function(key) {
 			localStorage.removeItem(key);
 		},
-
 		//View Complete Row by iteration key values
 		viewRow: function() {
 			for(key in localStorage) {
 				console.log(localStorage[key]);
 			}
 		},
-
 		//Create JSON Db or collection
 		storeJSON: function(key, json) {
 			localStorage.setItem(key, JSON.stringify(json));
 		},
-
 		//Read JSON from Db or Collections
 		readJSON: function(key) {
 			return JSON.parse(localStorage[key]);
 		},
-
 		//Secure Storage using session
 		createSecureRow : function (key, value) {
 			sessionStorage.setItem(key, value);
 		},
-
 		//Get Secure storage array
 		getSecureRow : function (key) {
 			return sessionStorage.getItem(key);
 		},
-
 		//Remove a secure row
 		removeSecureRow : function (key) {
 			sessionStorage.removeItem(key);
 		}
-
 	}	
 })(window);
